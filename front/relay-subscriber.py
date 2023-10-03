167d668c1678bb05af537b5bb7841a2fbc89cddd
RJG <richard.john.george.3@gmail.com>
2023-10-03 18:47:09 +0100

    cleaned up the on_nmessage to use a list

--- front/relay-subscriber.py
+++ front/relay-subscriber.py
@@ -32,11 +32,14 @@ topic_map = [
     'topic/lights/flasher'
 ]
 
-#to keep things simple the following values are used to index the light_map rows
-T_SIDE, T_HDIP, T_HFULL, T_LIND, T_RIND, T_HAZD, T_FFOG, T_SPOT, T_FLSH = range(9)
-
 # to keep things simple the following values are used to index the ight_map and state columns
 SIDE, HDIP, HFUL, LIND, RIND, FFOG, SPOT = range(7)
+
+# this is the pins that map to the lights in the order defined in the range above 
+pin_map = [11, 12, 22, 13, 15, 16, 18]
+
+# to keep things simple the following values are used to index the light_map rows
+T_SIDE, T_HDIP, T_HFULL, T_LIND, T_RIND, T_HAZD, T_FFOG, T_SPOT, T_FLSH = range(9)
 
 # for each event there is a possible combination of lights
 ON  = 0 # ON is the light shold br switched on
@@ -46,7 +49,7 @@ TMR = 3 # TMR starts a repeat timer of 0.75s ON and 0.75s OFF
 
 light_map = [
 #   [sid, dip, ful, lid, rid, fog, spt] 
-    [ON,  ON , OFF, OFF, OFF, OFF, OFF], # T_SIDE 'topic/lights/side'
+    [ON,  OFF, OFF, OFF, OFF, OFF, OFF], # T_SIDE 'topic/lights/side'
     [ASC, ON , OFF, OFF, OFF, OFF, OFF], # T_HDIP 'topic/lights/head-dipped'
     [ASC, OFF, ON , OFF, OFF, OFF, OFF], # T_HFUL 'topic/lights/head-full'
     [OFF, OFF, OFF, TMR, OFF, OFF, OFF], # T_LIND 'topic/lights/left-indicator'
@@ -82,89 +85,24 @@ def on_message(client, userdata, msg):
         print("some other erro rhappened with topic_map.index")
     else:
         print(light_map[topic_index])
-
-def on_message1(client, userdata, msg):
-    print(f"{msg.topic} {msg.payload}")
-    if msg.topic == 'topic/lights/side' and msg.payload == b'0':
-        GPIO.output(side, GPIO.HIGH)
-        print('turn side light on')
-    elif msg.topic == 'topic/lights/side' and msg.payload == b'1':
-        GPIO.output(side, GPIO.LOW)
-        print('turn side light on')
-    elif msg.topic == 'topic/lights/head-dipped' and msg.payload == b'0':
-        GPIO.output(headdip, GPIO.HIGH)
-        headdip_state = 0
-        print('turn head dipped light on')
-    elif msg.topic == 'topic/lights/head-dipped' and msg.payload == b'1':
-        GPIO.output(headdip, GPIO.LOW)
-        headdip_state = 1
-        print('turn head dipped light on')
-    elif msg.topic == 'topic/lights/head-full' and msg.payload == b'0':
-        GPIO.output(headfull, GPIO.HIGH)
-        headfull_state = 0
-        print('turn head full light on')
-    elif msg.topic == 'topic/lights/head-full' and msg.payload == b'1':
-        GPIO.output(headfull, GPIO.LOW)
-        headfull_state = 1
-        print('turn head full light on')
-    elif msg.topic == 'topic/lights/left-indicator' and msg.payload == b'0':
-        GPIO.output(leftind, GPIO.HIGH)
-        leftind_state = 0
-        print('turn left indicator light off')
-    elif msg.topic == 'topic/lights/left-indicator' and msg.payload == b'1':
-        GPIO.output(leftind, GPIO.LOW)
-        leftind_state = 1
-        print('turn left indicator light on')
-    elif msg.topic == 'topic/lights/right-indicator' and msg.payload == b'0':
-        GPIO.output(rightind, GPIO.HIGH)
-        rightind_state =0
-        print('turn right indicator light off')
-    elif msg.topic == 'topic/lights/right-indicator' and msg.payload == b'1':
-        GPIO.output(rightind, GPIO.LOW)
-        rightind_state = 1
-        print('turn right indicator light on')
-    elif msg.topic == 'topic/lights/hazards' and msg.payload == b'0':
-        GPIO.output(leftind, GPIO.HIGH)
-        GPIO.output(rightind, GPIO.HIGH)
-        print('turn hazards light off')
-    elif msg.topic == 'topic/lights/hazards' and msg.payload == b'1':
-        GPIO.output(leftind, GPIO.LOW)
-        GPIO.output(rightind, GPIO.LOW)
-        print('turn hazards light on')
-    elif msg.topic == 'topic/lights/front-fog' and msg.payload == b'0':
-        GPIO.output(fog, GPIO.HIGH)
-        print('turn fog light off')
-    elif msg.topic == 'topic/lights/front-fog' and msg.payload == b'1':
-        GPIO.output(fog, GPIO.LOW)
-        print('turn fog light on')
-    elif msg.topic == 'topic/lights/spot' and msg.payload == b'0':
-        GPIO.output(spot, GPIO.HIGH)
-        print('turn spot light off')
-    elif msg.topic == 'topic/lights/spot' and msg.payload == b'1':
-        GPIO.output(spot, GPIO.LOW)
-        print('turn spot light on')    
-    else:
-        print('unrecognised event')
-
+    
+        for light_index in range(len(light_map[topic_index])):
+            print ("light set to ", light_map[topic_index][light_index])
+            if light_map[topic_index][light_index] == ON:
+                if msg.payload == b'1': # turn on
+                    print ("turn on light ", pin_map[light_index])
+                    GPIO.output(pin_map[light_index], GPIO.LOW)
+                else: # turn off
+                    print ("turn off light ", pin_map[light_index])
+                    GPIO.output(pin_map[light_index], GPIO.HIGH)
+        
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)  # use the pin numbering over broadcom
-GPIO.setup(side, GPIO.OUT)
-GPIO.output(side, GPIO.HIGH)
-GPIO.setup(headdip, GPIO.OUT)
-GPIO.output(headdip, GPIO.HIGH)
-GPIO.setup(leftind, GPIO.OUT)
-GPIO.output(leftind, GPIO.HIGH)
-GPIO.setup(rightind, GPIO.OUT)
-GPIO.output(rightind, GPIO.HIGH)
-GPIO.setup(fog, GPIO.OUT)
-GPIO.output(fog, GPIO.HIGH)
-GPIO.setup(spot, GPIO.OUT)
-GPIO.output(spot, GPIO.HIGH)
-GPIO.setup(headfull, GPIO.OUT)
-GPIO.output(headfull, GPIO.HIGH)
-GPIO.setup(spare, GPIO.OUT)
-GPIO.output(spare, GPIO.HIGH)
+
+for pin in pin_map:
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.HIGH)
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -185,10 +123,12 @@ try:
     client.loop_forever()
 
 except KeyboardInterrupt:
-    print ("programme interupted")
+    print ("programme interupted by big fat fingers")
 
-except:
-    print ("some other interrpt")
+
+except Exception as error:
+    print ("some other interrpt - ", type(error).__name__, "–", error)
+
 
 finally:
     GPIO.cleanup()
