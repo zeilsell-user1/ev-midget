7e9c0db96538b5e7ad21f608fc5dc5c7a282de38
RJG <richard.john.george.3@gmail.com>
2023-10-05 16:20:26 +0100

    updated back controller code

--- back/relay-subscriber.py
+++ back/relay-subscriber.py
@@ -1,39 +1,110 @@
 # subscriber.py
 import paho.mqtt.client as mqtt
 import RPi.GPIO as GPIO
+import threading
 
 # define the MQTT broker to use
 
+#broker = "dashboard.hhw470d.local"
 broker = "192.168.2.2"
 myport = 1883
 
 # define the GPIO pins
 
-side = 11
-brake = 12
-leftind = 13
+brake    = 11
+side     = 12
+leftind  = 13
 rightind = 15
-fog = 16
-reverse = 18
-spare1 = 22
-spare2 = 7
+reverse  = 16
+fog      = 18
+spare12  = 22
+spare    = 7
+
+# list of the topics supported - order must match the range below
+topic_map = [
+    'topic/lights/brake',
+    'topic/lights/side',
+    'topic/lights/left-indicator',
+    'topic/lights/right-indicator',
+    'topic/lights/hazards',
+    'topic/lights/rear-fog',
+    'topic/lights/reversing',
+    'topic/lights/flasher'
+]
+
+# to keep things simple the following values are used to index the ight_map and state columns
+BRAK, SIDE, LIND, RIND, REVS, RFOG = range(6)
+
+# this is the pins that map to the lights in the order defined in the range above 
+pin_map = [11, 12, 13, 15, 16, 18]
+
+# to keep things simple the following values are used to index the light_map rows
+T_BRAK, T_SIDE, T_LIND, T_RIND, T_HAZD, T_RFOG, T_REVS = range(7)
+
+# for each event there is a possible combination of lights
+ON  = 0 # ON is the light shold br switched on
+OFF = 1 # OFF is the light should be switched off
+ASC = 2 # ASC is the light should br switched on, but only switch off if the specific light status is off
+TMR = 3 # TMR starts a repeat timer of 0.75s ON and 0.75s OFF
+
+light_map = [
+#   [brk, sid, lid, rid, rev, fog] 
+    [ON,  OFF, OFF, OFF, OFF, OFF], # T_SIDE 'topic/lights/brake'
+    [OFF, ON , OFF, OFF, OFF, OFF], # T_HDIP 'topic/lights/side'
+    [OFF, OFF, TMR, OFF, OFF, OFF], # T_LIND 'topic/lights/left-indicator'
+    [OFF, OFF, OFF, TMR, OFF, OFF], # T_RIND 'topic/lights/right-indicator'
+    [OFF, OFF, TMR, TMR, OFF, OFF], # T_HAZD 'topic/lights/hazards'
+    [OFF, OFF, OFF, OFF, OFF, ON ], # T_FFOG 'topic/lights/rear-fog'
+    [OFF, OFF, OFF, OFF, ON , OFF], # T_SPOT 'topic/lights/reversing'
+]
+
+state = [OFF,OFF,OFF,OFF,OFF,OFF]
+
+lind_timer = None
+lind_duty  = OFF
+rind_timer = None
+rind_duty  = OFF
+
+def on_lind_expire():
+    global lind_duty, lind_timer
+    if state[LIND] == ON:
+        if lind_duty == ON:
+            GPIO.output(pin_map[LIND], GPIO.LOW)
+            lind_duty = OFF
+        else:
+            GPIO.output(pin_map[LIND], GPIO.HIGH)
+            lind_duty = ON
+        lind_timer = threading.Timer(0.75, on_lind_expire)
+        lind_timer.start() 
+    else:
+        GPIO.output(pin_map[LIND], GPIO.HIGH)
+        state[LIND] = OFF
+        lind_timer = None
+        lind_duty = OFF
+
+def on_rind_expire():
+    global rind_duty, rind_timer
+    if state[RIND] == ON:
+        if rind_duty == ON:
+            GPIO.output(pin_map[RIND], GPIO.LOW)
+            rind_duty = OFF
+        else:
+            GPIO.output(pin_map[RIND], GPIO.HIGH)
+            rind_duty = ON
+        rind_timer = threading.Timer(0.75, on_rind_expire)
+        rind_timer.start() 
+    else:
+        GPIO.output(pin_map[RIND], GPIO.HIGH)
+        state[RIND] = OFF
+        rind_timer = None
+        rind_duty = OFF
 
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
-    # Subscribe, which need to put into on_connect
-    # If reconnect after losing the connection with the broker, it will continue to subscribe to the raspberry/topic topic
-    client.subscribe("topic/lights/rear/brake/on")
-    client.subscribe("topic/lights/rear/brake/off")
-    client.subscribe("topic/lights/rear/fog/on")
-    client.subscribe("topic/lights/rear/fog/off")
-    client.subscribe("topic/lights/rear/reverse/on")
-    client.subscribe("topic/lights/rear/reverse/off")
-    client.subscribe("topic/lights/rear/side/on")
-    client.subscribe("topic/lights/rear/side/off")
-    client.subscribe("topic/lights/rear/leftind/on")
-    client.subscribe("topic/lights/rear/leftind/off")
-    client.subscribe("topic/lights/rear/rightind/on")
-    client.subscribe("topic/lights/rear/rightind/off")
+    # If reconnect after losing the connection with the broker, it will continue to 
+    # subscribe to the raspberry/topic topic
+    for topic in topic_map:
+        client.subscribe(topic)
 
 #
 # The callback function, it will be triggered when receiving messages
@@ -42,86 +113,101 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
     print(f"{msg.topic} {msg.payload}")
-    if msg.topic == 'topic/lights/rear/reverse/on':
-        GPIO.output(reverse, GPIO.LOW)
-        print('turn reverse light on')
-    elif msg.topic == 'topic/lights/rear/reverse/off':
-        GPIO.output(reverse, GPIO.HIGH)
-        print('turn reverse light off')
-    elif msg.topic == 'topic/lights/rear/fog/on':
-        GPIO.output(fog, GPIO.LOW)
-        print('turn rear fog light on')
-    elif msg.topic == 'topic/lights/rear/fog/off':
-        GPIO.output(fog, GPIO.HIGH)
-        print('turn rear fog lights off')
-    elif msg.topic == 'topic/lights/rear/brake/on':
-        GPIO.output(brake, GPIO.LOW)
-        print('turn brake light dipped on')
-    elif msg.topic == 'topic/lights/rear/brake/off':
-        GPIO.output(brake, GPIO.HIGH)
-        print('turn brake lights dipped off')
-    elif msg.topic == 'topic/lights/rear/side/on':
-        GPIO.output(side, GPIO.LOW)
-        print('turn rear side light on')
-    elif msg.topic == 'topic/lights/rear/side/off':
-        GPIO.output(side, GPIO.HIGH)
-        print('turn rear side lights off')
-    elif msg.topic == 'topic/lights/rear/leftind/on':
-        GPIO.output(leftind, GPIO.LOW)
-        print('turn rear left indicator on')
-    elif msg.topic == 'topic/lights/rear/leftind/off':
-        GPIO.output(leftind, GPIO.HIGH)
-        print('turn rear left indicator off')
-    elif msg.topic == 'topic/lights/rear/rightind/on':
-        GPIO.output(rightind, GPIO.LOW)
-        print('turn rear right indicator on')
-    elif msg.topic == 'topic/lights/rear/rightind/off':
-        GPIO.output(rightind, GPIO.HIGH)
-        print('turn rear right indicator off')
-    else:
-        print('unrecognised event')
 
+    try:
+        topic_index = topic_map.index(msg.topic)
+    except ValueError:
+        print("topic was not found in subscribable list")
+    except:
+        print("some other erro rhappened with topic_map.index")
+    else:
+        for light_index in range(len(light_map[topic_index])):
+            if light_map[topic_index][light_index] == ON:
+                if msg.payload == b'1': # turn on
+                    print ("turn on light ", pin_map[light_index])
+                    GPIO.output(pin_map[light_index], GPIO.LOW)
+                    state[light_index] = ON
+                else: # turn off
+                    print ("turn off light ", pin_map[light_index])
+                    GPIO.output(pin_map[light_index], GPIO.HIGH)
+                    state[light_index]= OFF
+
+            if light_map[topic_index][light_index] == OFF: # shouldn't happen but JIC
+                if state[light_index] != ON: # only turn off if not on by controler switch
+                    GPIO.output(pin_map[light_index], GPIO.HIGH)
+
+            if light_map[topic_index][light_index] == ASC:
+                if msg.payload == b'1': # turn on
+                    print ("turn on light ", pin_map[light_index])
+                    GPIO.output(pin_map[light_index], GPIO.LOW)
+                else: # turn off
+                    if state[light_index] == OFF: # only turn off if not on by controler switch
+                        print ("turn off light as not state on ", pin_map[light_index])
+                        GPIO.output(pin_map[light_index], GPIO.HIGH)
+
+            if light_map[topic_index][light_index] == TMR:
+                if msg.payload == b'1': # turn on
+                    print ("turn on light ", pin_map[light_index])
+                    GPIO.output(pin_map[light_index], GPIO.LOW)
+                    if light_index == LIND:
+                        lind_timer = threading.Timer(0.75, on_lind_expire)
+                        lind_timer.start() 
+                        state[light_index] = ON
+                        lind_duty = ON
+                    elif light_index == RIND:
+                        rind_timer = threading.Timer(0.75, on_rind_expire)
+                        rind_timer.start() 
+                        state[light_index] = ON
+                        rind_duty = ON
+                    else:
+                        print("Error: TMR set butnot for an indicator")
+                else: # turn off
+                    print ("turn off light ", pin_map[light_index])
+                    GPIO.output(pin_map[light_index], GPIO.HIGH)
+                    if light_index == LIND:
+                        state[light_index] = OFF
+                    elif light_index == RIND:
+                        state[light_index] = OFF
+                    else:
+                        print("Error: TMR set but not for an indicator")
+
+
+
+
+        
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)  # use the pin numbering over broadcom
-GPIO.setup(side, GPIO.OUT)
-GPIO.output(side, GPIO.HIGH)
-GPIO.setup(brake, GPIO.OUT)
-GPIO.output(brake, GPIO.HIGH)
-GPIO.setup(leftind, GPIO.OUT)
-GPIO.output(leftind, GPIO.HIGH)
-GPIO.setup(rightind, GPIO.OUT)
-GPIO.output(rightind, GPIO.HIGH)
-GPIO.setup(fog, GPIO.OUT)
-GPIO.output(fog, GPIO.HIGH)
-GPIO.setup(reverse, GPIO.OUT)
-GPIO.output(reverse, GPIO.HIGH)
-GPIO.setup(spare1, GPIO.OUT)
-GPIO.output(spare1, GPIO.HIGH)
-GPIO.setup(spare2, GPIO.OUT)
-GPIO.output(spare2, GPIO.HIGH)
+
+for pin in pin_map:
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.HIGH)
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-# Set the will message, when the Raspberry Pi is powered off, or the network is interrupted abnormally, it will send the will message to other clients
-client.will_set('raspberry/status', b'{"status": "Off"}')
+# Set the will message, when the Raspberry Pi is powered off, or 
+# the network is interrupted abnormally, it will send the will message t
+# o other clients
+client.will_set('raspberry/back/status', b'{"status": "Off"}')
 
-# Create connection, the three parameters are broker address, broker port number, and keep-alive time respectively
+# Create connection, the three parameters are broker address, 
+# broker port number, and keep-alive time respectively
 client.connect(broker, myport, 60)
 
-# Set the network loop blocking, it will not actively end the program before calling disconnect() or the program crash
-client.loop_forever()
-
+# Set the network loop blocking, it will not actively end the program 
+# before calling disconnect() or the program crash
 try:
     client.loop_forever()
 
 except KeyboardInterrupt:
-    print ("programme interupted")
+    print ("programme interupted by big fat fingers")
 
-except:
-    print ("some other interrpt")
+
+except Exception as error:
+    print ("some other interrpt - ", type(error).__name__, "–", error)
+
 
 finally:
     GPIO.cleanup()
