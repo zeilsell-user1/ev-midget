828e89a78cf88bde120becc69e1a54c7bee99738
Richard George <richard.jhn.george.3@gmail.com>
2023-10-10 18:35:21 +0100

    added the first of the ESP8266 builds

---
+++ esp01-projects/wifi-hotspot/src/main.cpp
@@ -0,0 +1,41 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+
+const char *ssid = "hhw470d";
+const char *password = "Target21#";
+
+IPAddress local_IP(192,168,3,0);
+IPAddress gateway(192,168,3,1);
+IPAddress subnet(255,255,255,0);
+
+ESP8266WebServer server(80);
+
+void handleRoot() {
+  server.send(200, "text/html", "<h1>You are connected</h1>");
+}
+
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+
+  // start the wifi hotspot
+  Serial.print("Setting soft-AP configuration ... ");
+  Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
+  WiFi.softAP(ssid, password);
+
+  // retrive the IP address of the hotspot
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+
+  // start the HTTP server 
+  server.on("/", handleRoot);
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop() {
+  server.handleClient();
+}
+
